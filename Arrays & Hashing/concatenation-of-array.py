3744376bb9cfbf5405d811d97bdc5d4e7c07e67b
Jionghao Wu <jhwu03@gmail.com>
2024-08-20 01:08:38 -0400

    Finished concatenation of array

---
+++ Arrays & Hashing/concatenation-of-array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        x = len(nums)
+        for i in range(x):
+            nums.append(nums[i])
+        return nums 
+
+        
